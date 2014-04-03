035fae55fff04ba1fe0a3d0d513c93a8b4b7ec96
ptjoker95 <ptjoker95@gmail.com>
2014-04-03 17:18:55 +0900

    알고리즘을 바꿔야 될 것 같음. 애초에 라인을 그리디알고리즘으로

--- solver.py
+++ solver.py
@@ -4,7 +4,20 @@
 import math
 from collections import namedtuple
 
+import numpy as np
+
 Point = namedtuple("Point", ['x', 'y'])
+
+#각 edge들의 거리에 관한 리스트를 리턴하는 함수
+def LenofEdges(solution, points, nodeCount):
+    LengthofEachRoute = [0]*nodeCount
+    
+    for index in range(0, nodeCount-1):
+        LengthofEachRoute[index] = length(points[solution[index]], points[solution[index+1]])
+    LengthofEachRoute[-1] = length(points[solution[-1]], points[solution[0]])
+    
+    return LengthofEachRoute
+
 
 # calculate the length of the tour
 def CalculateObj(points, solution, nodeCount):
@@ -16,22 +29,26 @@ def CalculateObj(points, solution, nodeCount):
 
 #어떤 노드에서 가장 가까이에 있는 노드를 리턴해준다.
 def NodeofNearest(points, SwappingPoint, nodeCount):
-    eachlength = [0]*nodeCount
+    #일단 입력받은 노드에서 각 노드간의 거리를 계산한 뒤 리스트에 넣는다.
+    minvalue = 0
+    minnode = 0
+    
     for i, point in enumerate(points):
-        if( i is not SwappingPoint ):
-            eachlength[i] = length(point, points[SwappingPoint])
-        else:
-            eachlength[i] = 0
-    
-    minvalue = eachlength[0]
-    
-    for e in eachlength:
-        if e is not 0:
-            if minvalue > e:
-                minvalue = e
-        
-    return eachlength.index(minvalue)
+        tempvalue = length(point, points[SwappingPoint])
+        #print "tempvalue: ", tempvalue
+        if( minvalue is 0 and tempvalue > 0):
+            #print "minvalue is 0 and tempvalue is not 0"
+            minvalue = tempvalue
+            minnode = i 
+        if( minvalue > tempvalue and tempvalue > 0):
+            #print "minvalue > tempvalue"
+            minvalue = tempvalue
+            minnode = i    
 
+    #print "minvalue: ", minvalue, " minnode: ", minnode
+    
+    return minnode
+    
 
 # 가장 현재식에서 가장 거리가 긴 루트를 찾아, 그 인덱스를 리턴한다.
 
@@ -59,32 +76,46 @@ def solve_it(input_data):
     # visit the nodes in the order they appear in the file
     solution = range(0, nodeCount)
     
+    # 임시 값을 넣어둘 리스트
+    tempsolution = solution
+    
     # 각 node에서 다음 node까지의 거리리스트
-    LengthofEachRoute = [0]*nodeCount
+    #LengthofEachRoute = [0]*nodeCount
     
     # calculate the length of the tour
-    obj = length(points[solution[-1]], points[solution[0]])
-    for index in range(0, nodeCount-1):
-        obj += length(points[solution[index]], points[solution[index+1]])
-        LengthofEachRoute[index] = length(points[solution[index]], points[solution[index+1]])
-    LengthofEachRoute[-1] = length(points[solution[-1]], points[solution[0]])
+    obj = CalculateObj(points, solution, nodeCount)
+    #LengthofEachRoute = LenofEdges(solution, points, nodeCount)
+        
+    # node 갯수만큼 루프를 돌리면서 최적해를 찾아본다.
+    for i in solution:
+        # 가장 거리가 긴 루트를 찾고 인덱스를 뽑아온다.   
+         
+        SwappingPoint1 = i
     
-    # 가장 거리가 긴 루트를 찾고 인덱스를 뽑아온다.    
-    SwappingPoint1 = IndexofMaxLengthRoute(LengthofEachRoute)
+        # 그 node에서 가장 짧은 거리에 있는 node를 찾아본다.
+        SwappingPoint2 = NodeofNearest(points, SwappingPoint1, nodeCount)
     
-    # 그 node에서 가장 짧은 거리에 있는 node를 찾아본다.
-    SwappingPoint2 = NodeofNearest(points, SwappingPoint1, nodeCount)
+        #
+        #print "Swapping Point 1: ", SwappingPoint1, " Swapping Point 2: ", SwappingPoint2
+        #서로 바꿔준다.
+        tempindex1 = solution.index(SwappingPoint1)
+        tempindex2 = solution.index(SwappingPoint2)
+        
+        tempsolution[tempindex1] = SwappingPoint2
+        tempsolution[tempindex2] = SwappingPoint1
     
-    #서로 바꿔준다.
-    solution[SwappingPoint1] = SwappingPoint2
-    solution[SwappingPoint2] = SwappingPoint1
-    
+        tempObj = CalculateObj(points, tempsolution, nodeCount)
+        
+        if tempObj < obj:
+            solution = tempsolution
+            #LengthofEachRoute = LenofEdges(solution, points, nodeCount)
+            obj = CalculateObj(points, solution, nodeCount)
     # 다시 계산
-    obj = length(points[solution[-1]], points[solution[0]])
-    for index in range(0, nodeCount-1):
-        obj += length(points[solution[index]], points[solution[index+1]])
-        LengthofEachRoute[index] = length(points[solution[index]], points[solution[index+1]])
-    LengthofEachRoute[-1] = length(points[solution[-1]], points[solution[0]])
+    #obj = length(points[solution[-1]], points[solution[0]])
+    #for index in range(0, nodeCount-1):
+        #obj += length(points[solution[index]], points[solution[index+1]])
+        #LengthofEachRoute[index] = length(points[solution[index]], points[solution[index+1]])
+    #LengthofEachRoute[-1] = length(points[solution[-1]], points[solution[0]])
     
     
     # prepare the solution in the specified output format
